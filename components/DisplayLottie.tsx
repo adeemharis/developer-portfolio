d9ce9ffa96714ac86561a896efb224504922b589
Adeem Haris <adeemharis@gmail.com>
2024-12-03 17:30:18 +0530

    Update diplaylottie

--- components/DisplayLottie.tsx
+++ components/DisplayLottie.tsx
@@ -1,49 +1,23 @@
-// import React from "react";
-// import Lottie from "react-lottie";
+import React from "react";
+import Lottie from "react-lottie";
 
-// type Props = {
-//   animationPath: string;
-// };
-
-// const GreetingLottie = ({ animationPath }: Props) => {
-//   const defaultOptions = {
-//     loop: true,
-//     autoplay: true,
-//     path: animationPath,
-//   };
-
-//   return (
-//     <div onClick={() => null}>
-//       {/* @ts-ignore */}
-//       <Lottie options={defaultOptions} />
-//     </div>
-//   );
-// };
-
-// export default GreetingLottie;
-import dynamic from "next/dynamic";
-
-const Lottie = dynamic(() => import("react-lottie"), { ssr: false });
-
-type DisplayLottieProps = {
+type Props = {
   animationPath: string;
 };
 
-const DisplayLottie = ({ animationPath }: DisplayLottieProps) => {
-  if (typeof window === "undefined") {
-    return null; // Render nothing on the server
-  }
-
+const GreetingLottie = ({ animationPath }: Props) => {
   const defaultOptions = {
     loop: true,
     autoplay: true,
-    animationData: require(`../animations/${animationPath}`),
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
+    path: animationPath,
   };
 
-  return <Lottie options={defaultOptions} height={300} width={300} />;
+  return (
+    <div onClick={() => null}>
+      {/* @ts-ignore */}
+      <Lottie options={defaultOptions} />
+    </div>
+  );
 };
 
-export default DisplayLottie;
+export default GreetingLottie;
